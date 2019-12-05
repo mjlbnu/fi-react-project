d942cacaa06d1ca74aa4b7a428ed88379497a351
mjlbnu <mjlbnu@gmail.com>
2019-12-04 22:25:27 -0300

    Exibindo repositório

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,29 +1,5 @@
 import styled, { keyframes, css } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  margin: 80px auto;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  .git {
-    margin-right: 10px;
-  }
-
-  svg {
-    margin-right: 0px;
-  }
-`;
-
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
